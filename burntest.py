16f7e885164b3cce3f8ec7115b607f47f46648a5
matthew venn <matt@mattvenn.net>
2013-06-24 17:10:49 +0100

    burntest for testing batteries

---
+++ burntest.py
@@ -0,0 +1,28 @@
+import buggy
+import time
+import sys
+
+"""
+little demo to show buggy control via python
+"""
+
+buggy.stop_all()
+max_speed = 60 
+if len(sys.argv)==2:
+    max_speed = sys.argv[1]
+    print "maxspeed:", max_speed
+
+pause_time = 1 #second
+drive_time = 1 #second
+
+while True:
+
+    #straight line
+    print "straight"
+    buggy.drive(buggy.LEFT,max_speed)
+    buggy.drive(buggy.RIGHT,max_speed)
+    time.sleep(drive_time)
+    buggy.stop_all()
+
+    print "pause"
+    time.sleep(pause_time)
